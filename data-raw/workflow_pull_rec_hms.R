bffbaff617b4d1ebd93851ddb2748a80cffb63e6
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2025-09-17 11:41:02 -0400

    roxygenise, add channel parameter

--- data-raw/workflow_pull_rec_hms.R
+++ data-raw/workflow_pull_rec_hms.R
@@ -1,8 +1,9 @@
 #' Pull recreational HMS required for indicator creation
 #'
-#' @param outputDir Character string. Path to folder where data pull should be saved. 
+#' @param outputDir Character string. Path to folder where data pull should be saved.
 #' If not NULL the pull will be saved to the
-#' folder `//nefscdata/EDAB_Dev/atyrell/hms_mrip_data/` 
+#' folder `//nefscdata/EDAB_Dev/atyrell/hms_mrip_data/`
+#' @param channel Oracle connection channel created with `dbutils`
 #'
 #' @return rec_hms_data, a csv file with landings data from MRIP
 #' @return list of data objects. rds files exported
@@ -12,14 +13,14 @@
 #'   workflow_pull_rec_hms(outputDir)
 #' }
 #'
-workflow_pull_rec_hms <- function(outputDir = NULL) {
+workflow_pull_rec_hms <- function(outputDir = NULL, channel) {
   # pull MRIP data
   rec_hms_data <- SOEworkflows::pull_rec_hms(channel)
-  
+
   # Save these to a specific location
   if (!is.null(outputDir)) {
     saveRDS(rec_hms_data, paste0(outputDir, "hms_mrip_", Sys.Date(), ".csv"))
   }
-  
+
   return(rec_hms_data)
 }
