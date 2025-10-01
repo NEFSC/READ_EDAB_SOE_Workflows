dc923957be27cdd2b308f1372bdb87187fc0ecd4
andybeet <22455149+andybeet@users.noreply.github.com>
2025-10-01 11:00:40 -0500

    renamed main function to align with ecodata name

--- R/mass_inshore_survey.R
+++ R/mass_inshore_survey.R
@@ -1,123 +1,41 @@
-#' Get Massachussets inshore bottom trawl survey data 
-#'
-#' @description
-#' Pulls survey data in format required for indicator generation function
-#'
-#' @param channel to connect to communicate with the database engine.
-#'
-#'@return Mass inshore survey data pull
-#'
-#'@examples
-#'\dontrun{
-#'channel <- dbutils::connect_to_database("server",user)
-#'rawData <- get_mass_survey(channel)
-#'}
-#'
-#'@export
-
-# Helper function----
-sqltext <- function(x){
-  out <- x[1]
-  if(length(x) > 1){
-    for(i in 2:length(x)){
-      out <- paste(out, x[i], sep = "','")
-    }
-  }
-  out <- paste("'", out, "'", sep = '')
-  return(out)
-}
-
-# Main function
-get_mass_survey <- function(channel){
-  channel <- dbutils::connect_to_database("NEFSC_pw_oraprod","amolina")  
-  cruise.qry <- "select unique year, cruise6, svvessel, season
-                 from svdbs.mstr_cruise
-                 where purpose_code = 11
-                 and year >= 1963
-                 order by year, cruise6"
-  
-  
-  cruise <- data.table::as.data.table(DBI::dbGetQuery(channel, cruise.qry))
-  cruise <- na.omit(cruise)
-  
-  #Use cruise codes to select other data
-  cruise6 <- sqltext(cruise$CRUISE6)
-  
-  #Station data
-  station.qry <- "select unique cruise6, svvessel, station, stratum, tow,
-                        beglat as lat, beglon as lon,
-                        begin_est_towdate as est_towdate, avgdepth as depth,
-                        surftemp, bottemp,surfsalinity,botsalinity
-                        from svdbs.fscs_svsta
-                        where cruise6 = cruise6
-                        and SHG <= 136
-                        order by cruise6, station"
-  
-  station <- data.table::as.data.table(DBI::dbGetQuery(channel, station.qry))
-  
-  #merge cruise and station
-  survdat.mass <- merge(cruise, station)
-  
-  #Catch data
-  catch.qry <- "select cruise6, station, stratum, tow, svspp, catchsex,
-                     expcatchnum as abundance, expcatchwt as biomass
-                     from svdbs.FSCS_SVCAT
-                     where cruise6 = cruise6
-                     and stratum not like 'YT%'
-                     order by cruise6, station, svspp"
-  
-  catch <- data.table::as.data.table(DBI::dbGetQuery(channel, catch.qry))
-  
-  #merge with survdat
-  data.table::setkey(survdat.mass, CRUISE6, STATION, STRATUM, TOW)
-  survdat.mass <- merge(survdat.mass, catch, by = data.table::key(survdat.mass))
-  
-  #Convert number fields from chr to num
-  numberCols <- c('CRUISE6', 'STATION', 'STRATUM', 'TOW', 'SVSPP', 'CATCHSEX', 'YEAR')
-  survdat.mass[, (numberCols):= lapply(.SD, as.numeric), .SDcols = numberCols][]
-  return(survdat.mass)
-}
-
-
 #' Function to process Massachusetts inshore bottom trawl survey for automated workflow
 #'
 #' Data include aggregated time series of inshore fishery-independent trawl survey data from Massachusetts waters.
 #' MA inshore surveys have been performed biannually in the spring and fall since 1978.
 #'
-#' @param inputPathMassSurvey Character string. Full path to the mass inshore data pull rds file created by get_mass_survey
+#' @param inputPathMassSurvey Character string. Full path to the mass inshore data pull rds file created by workflow_pull_survey_data
 #' @param inputPathSpecies Character string. Full path to the species list data pull rds file
 #' 
 #'
 #' @examples
 #' \dontrun{
-#' # create the ecodata::ma_inshore_survey indicator
-#' create_mass_inshore_survey(inputPathSurvey <- here::here("survdat.mass.rds"), 
+#' # create the ecodata::mass_inshore_survey indicator
+#' create_mass_inshore_survey(inputPathMassSurvey <- here::here("mass_inshore.rds"), 
 #'                          inputPathSpecies <- "/home/<user>/EDAB_Datasets/SOE_species_list_24.rds")
 #'
 #' }
 #'
 #'
-#' @return ecodata::ma_inshore_survey data frame
+#' @return ecodata::mass_inshore_survey data frame
 #'
 #' @export
 
-######### 
-# packages --> data.table, survdat, sf,NEFSCspatial
-
-create_mass_inshore_survey <- function(inputPathSurvey, inputPathSpecies) {
+create_mass_inshore_survey <- function(inputPathMassSurvey, inputPathSpecies) {
   
   end.year <- format(Sys.Date(),"%Y")
 
   # Read survey data & species-------------------------------------------
-  survdat.mass <- readRDS(inputPathSurvey)
+  survdat.mass <- readRDS(inputPathMassSurvey)$survdat |> 
+    dplyr::filter(YEAR <= end.year)
   
   # Read species list -------------
-  Spp.list <- readRDS(inputPathSpecies) 
+  species <- readRDS(inputPathSpecies) 
   
-  # Aggregate species----
-  
+  # Find unique species codes and groups---
+  uniqueSpecies <- species |> 
+    dplyr::distinct(SVSPP,SOE.24)
   #Merge with species list to get species groups
-  survdat.mass <- merge(survdat.mass, unique(Spp.list[, list(SVSPP, SOE.24)]),
+  survdat.mass <- merge(survdat.mass, uniqueSpecies,
                         by = 'SVSPP', all.x = T)
   
   #Grab strata 
@@ -172,7 +90,8 @@ create_mass_inshore_survey <- function(inputPathSurvey, inputPathSpecies) {
   # Prepare for SOE ------
 
   #Remove missing values
-  stratmeanData <- stratmeanData[!is.na(stratmeanData$SOE.24), ]
+  #stratmeanData <- stratmeanData[!is.na(stratmeanData$SOE.24), ]
+  stratmeanData <- stratmeanData[!is.na(SOE.24), ]
   
   # select biomass and biomass SE for both spring and fall for each guild
   mass.survey <- stratmeanData |>
@@ -197,9 +116,9 @@ create_mass_inshore_survey <- function(inputPathSurvey, inputPathSpecies) {
   # fill in Time, Var, EPUs that are missing
   expanded <- expand.grid(Time = min(mass.survey$Time):end.year,Var = unique(mass.survey$Var))
   mass.survey <- expanded |>
-    dplyr::left_join(mass.survey, by = c("Time", "Var"))
-    #dplyr::as_tibble() |> 
-    #dplyr::relocate(Time,Var,Value,EPU,Units)
+    dplyr::left_join(mass.survey, by = c("Time", "Var")) |> 
+    dplyr::mutate(EPU = "GB") |> 
+    dplyr::relocate(Time,Var,Value,EPU,Units)
 
   return(mass.survey)
   
