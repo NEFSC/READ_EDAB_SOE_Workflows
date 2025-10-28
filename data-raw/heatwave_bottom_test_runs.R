7c2908aba678e51260fe795fbe37ffe5470cc9b7
Brandon Beltz - NOAA Affiliate <136381970+BBeltz1@users.noreply.github.com>
2025-10-28 13:35:15 -0400

    refactor(heatwave): change file paths to network drive locations for containerized testing

--- data-raw/heatwave_bottom_test_runs.R
+++ data-raw/heatwave_bottom_test_runs.R
@@ -1,10 +1,10 @@
 # Example of how to test on the Rstudio container 
 
 # suite of paths to input and output files
-outputPath <- here::here()
-inputPathGB <- here::here("daily_bottomT_GB_1959_2024_detrended.csv")
-inputPathGOM <- here::here("daily_bottomT_GOM_1959_2024_detrended.csv")
-inputPathMAB <- here::here("daily_bottomT_MAB_1959_2024_detrended.csv")
+outputPath <- "/home/bbeltz/EDAB_Dev/beltz/"
+inputPathGB <- "/home/bbeltz/EDAB_Dev/beltz/daily_bottomT_GB_1959_2024_detrended.csv"
+inputPathGOM <- "/home/bbeltz/EDAB_Dev/beltz/daily_bottomT_GOM_1959_2024_detrended.csv"
+inputPathMAB <- "/home/bbeltz/EDAB_Dev/beltz/daily_bottomT_MAB_1959_2024_detrended.csv"
 
 # source workflow functions from data-raw since they are not accessible from the package installation
 source(here::here("data-raw/workflow_heatwave_bottom.R"))
--- data-raw/heatwave_surface_test_runs.R
+++ data-raw/heatwave_surface_test_runs.R
@@ -1,10 +1,10 @@
 # Example of how to test on the Rstudio container 
 
 # suite of paths to input and output files
-outputPath <- here::here()
-inputPathGB <- here::here("GB_SST_1982_to_2024_detrended.csv")
-inputPathGOM <- here::here("GOM_SST_1982_to_2024_detrended.csv")
-inputPathMAB <- here::here("MAB_SST_1982_to_2024_detrended.csv")
+outputPath <- "/home/bbeltz/EDAB_Dev/beltz/"
+inputPathGB <- "/home/bbeltz/EDAB_Dev/beltz/GB_SST_1982_to_2024_detrended.csv"
+inputPathGOM <- "/home/bbeltz/EDAB_Dev/beltz/GOM_SST_1982_to_2024_detrended.csv"
+inputPathMAB <- "/home/bbeltz/EDAB_Dev/beltz/MAB_SST_1982_to_2024_detrended.csv"
 
 # source workflow functions from data-raw since they are not accessible from the package installation
 source(here::here("data-raw/workflow_heatwave_surface.R"))
