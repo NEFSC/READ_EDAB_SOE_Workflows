80b919c4b15f9fdbd80baf64d1382620f3a77acf
Max Grezlik <maxwell.grezlik@noaa.gov>
2025-10-30 10:56:46 -0500

    updated stockSmart section to more closely match method in old workflow. Deleted commented out sections

--- R/productivity_anomaly.R
+++ R/productivity_anomaly.R
@@ -50,9 +50,7 @@ species2out <- readRDS(inputPathSpecies) |>
 
 
 # load survey data -----------------------
-# commenting out for now. I don't think 2-load.R uses bio without epu
-# survey_data <- readRDS(input_survey_bio)
-# survey_data <- survey_data$survdat
+message("Loading survey data")
 survey_data_epu <- readRDS(input_survey_bio_epu) |> 
                       # convert columns to match trawlr function
                       dplyr::mutate(
@@ -63,41 +61,14 @@ survey_data_epu <- readRDS(input_survey_bio_epu) |>
 
 species <- readRDS(inputPathSpecies)
 
-# survdata <- survey_data |> 
-#   dplyr::left_join(species, by = 'SVSPP', relationship = "many-to-many") |> 
-#   dplyr::filter(SCINAME %in% species2out)
-
 survdata_epu <- survey_data_epu |> 
   dplyr::left_join(species, by = 'SVSPP', relationship = "many-to-many") |> 
   dplyr::filter(SCINAME %in% species2out)
-
-# Convert into recruitment estimates (whole shelf) ----------------
-# https://github.com/NOAA-EDAB/trawlr/blob/392fdc6e841893bdfe2322abe6801cc695e04312/R/func-data.R#L831
-message("recruitment estimates - whole shelf")
-
-# calc_rec(survdat = survdat,
-#          min_year = 1980,
-#          max_year = 2022,
-#          by_epu   = FALSE,
-#          by_taxon = FALSE,
-#          len_cutoff = 0.2)
 
 # Estimate the length cut-off for age-1 fish
 # https://github.com/NOAA-EDAB/trawlr/blob/392fdc6e841893bdfe2322abe6801cc695e04312/R/func-data.R#L771
 
 message("estimating length cut-off for age-1 fish")
-
-# df_len_at_age1 <- survdata  |> 
-#   dplyr::filter(YEAR >= (1980 - 1),
-#                 YEAR <= end.year,
-#                 !is.na(AGE),
-#                 !is.na(LENGTH)) |> 
-#   dplyr::group_by(COMNAME, SCINAME)  |> 
-#   dplyr::summarise(length_at_age1 = {
-#     len_model <- glm(LENGTH ~ AGE, family = "poisson")
-#     as.numeric(predict(len_model, newdata = data.frame(AGE = 2), type = "response"))
-#   }, .groups = "drop")
-
 
 df_len_at_age1_epu <- survdata_epu  |> 
   dplyr::filter(YEAR >= (1980 - 1),
@@ -113,11 +84,6 @@ df_len_at_age1_epu <- survdata_epu  |>
 # Calculate the number of tows in each survey season
 message("calculating number of tows per cruise/year/season")
 
-# dat_tows <- survdata  |>
-#   dplyr::distinct(CRUISE6, YEAR, SEASON, STATION, STRATUM)  |>
-#   dplyr::group_by(CRUISE6, YEAR, SEASON)  |>
-#   dplyr::summarise(n_tows = dplyr::n(), .groups = "drop")
-
 dat_tows_epu <- survdata_epu  |> 
   dplyr::distinct(CRUISE6, YEAR, SEASON, STATION, STRATUM)  |> 
   dplyr::group_by(CRUISE6, YEAR, SEASON)  |> 
@@ -125,7 +91,7 @@ dat_tows_epu <- survdata_epu  |>
 
 # merge and calculate recruitment-related variables ---------------------
 
-message("Calculating recruitment-related variables (EPU)")
+message("Calculating recruitment-related variables")
 
 dat_spec_rec_epu <- survdata_epu |> 
   dplyr::left_join(dat_tows_epu, by = c("CRUISE6", "YEAR", "SEASON")) |> 
@@ -144,25 +110,6 @@ dat_spec_rec_epu <- survdata_epu |>
     ind_sp_wl = ifelse(maturity == "spawner", WTperLENGTH, NaN),
     ind_rec_wl = ifelse(maturity == "recruit", WTperLENGTH, NaN)
   )
-
-# dat_spec_rec <- survdata |> 
-#   dplyr::left_join(dat_tows_epu, by = c("CRUISE6", "YEAR", "SEASON")) |> 
-#   dplyr::left_join(df_len_at_age1_epu, by = c("COMNAME", "SCINAME")) |> 
-#   dplyr::filter(YEAR >= (1980 - 1), YEAR <= end.year) |> 
-#   dplyr::mutate(
-#     NUMLEN = as.numeric(NUMLEN),
-#     INDWT  = as.numeric(INDWT),
-#     WTperLENGTH = INDWT / LENGTH,
-#     rank_LENGTH = dplyr::cume_dist(LENGTH),
-#     maturity = ifelse(is.na(length_at_age1),
-#                       ifelse(rank_LENGTH > 0.2, "spawner", "recruit"),
-#                       ifelse(LENGTH > length_at_age1, "spawner", "recruit")),
-#     ind_sp_abund = ifelse(maturity == "spawner", NUMLEN, 0),
-#     ind_rec_abund = ifelse(maturity == "recruit", NUMLEN, 0),
-#     ind_sp_wl = ifelse(maturity == "spawner", WTperLENGTH, NaN),
-#     ind_rec_wl = ifelse(maturity == "recruit", WTperLENGTH, NaN)
-#   )
-
 
 # summarize by species (whole shelf) ---------------------
 message("Summarizing whole-shelf for SOE")
@@ -241,65 +188,180 @@ productivity_anomaly1 <- dat_spec_rec_forSOE  |>
 
 message("Pulling and tidying stockSMART data")
 
-prod_assess1 <- stocksmart::stockAssessmentData |>
-  dplyr::filter(RegionalEcosystem == "Northeast Shelf") |>
-  dplyr::group_by(StockName, Metric, Year) |>
+# following https://github.com/NOAA-EDAB/stockstatusindicator/blob/master/MultisppRec2024.Rmd
+# old script adds prelim assessment data. I am leaving out for now
+
+## get stocksmart info -----------------------
+
+NErec <- stocksmart::stockAssessmentData  |> 
+  filter(RegionalEcosystem == "Northeast Shelf",
+         Metric == "Recruitment") |> 
+  group_by(StockName)  |> 
+  filter(AssessmentYear == max(AssessmentYear))  |> 
+  ungroup() 
+
+NErec_sppunits <- NErec  |> 
+  select(StockName, StockArea, Description, Units) |> 
+  distinct()
+
+NEbio <- stocksmart::stockAssessmentData  |> 
+  filter(RegionalEcosystem == "Northeast Shelf",
+         Metric == "Abundance")  |> 
+  group_by(StockName) |> 
+  filter(AssessmentYear == max(AssessmentYear)) |> 
+  ungroup() 
+
+NEbio_sppunits <- NEbio |> 
+  select(StockName, StockArea, Description, Units) |> 
+  distinct()
+
+SSByr <- NEbio  |> 
+  group_by(StockName) |> 
+  select(StockName, AssessmentYear) |> 
+  filter(AssessmentYear>2018) |> 
+  distinct()
+
+recyr <- NErec  |> 
+  group_by(StockName) |> 
+  select(StockName, AssessmentYear) |> 
+  filter(AssessmentYear>2018) |> 
+  distinct()
+
+NErecN <- NErec  |> 
+  filter(AssessmentYear>2018,
+         Units %in% c("Thousand Recruits",
+                      "Number x 1,000,000",
+                      "Number x 1,000",
+                      "Million Recruits")) 
+
+NErecstocks <- NErecN |> 
+  select(StockName) |> 
+  distinct()
+
+NEbiohasrec <- NEbio  |> 
+  filter(StockName %in% NErecstocks$StockName)
+
+bothyr <- NEbiohasrec  |> 
+  group_by(StockName) |> 
+  select(StockName, AssessmentYear) |> 
+  distinct()
+
+
+## standardize assessment outputs ------------
+
+NErecNstd <- NErecN  |> 
+  mutate(NfishRec = case_when(Units == "Thousand Recruits" ~ Value*1000,
+                              Units == "Number x 1,000,000" ~ Value*1000000,
+                              Units == "Number x 1,000" ~ Value*1000,
+                              Units == "Million Recruits" ~ Value*1000000,
+                              TRUE ~ as.numeric(NA)),
+         AgeRec = as.numeric(stringr::str_extract(Description, "(\\d)+")),
+         SSByr = Year-AgeRec
+  )
+
+## convert spiny dog -----------------------
+# In 2023 the MT assessment for spiny dogfish now reports stock size as reproductive potential in million pups. 
+# In order to keep spiny dogfish in the mix for this analysis, I am converting this to biomass units assuming that a pup weighs 0.5 kg. 
+# We are calculating an anomaly of recruits per biomass so I think this is ok even though it is not a measure of adult biomass.
+
+NEbioBstd <- NEbiohasrec  |> 
+  mutate(SSBkg = case_when(Units == "Thousand Metric Tons" ~ Value*1000000,
+                           Units == "Metric Tons" ~ Value*1000,
+                           Units == "Million Pups" ~ Value*10000000/2,
+                           TRUE ~ as.numeric(NA))
+         #AgeRec = as.numeric(stringr::str_extract(Description, "(\\d)+")),
+         #SSByr = Year-AgeRec
+  )
+
+
+## Perretti anomaly calculation ---------------------------------------
+
+SSrecbio <- NErecNstd  |> 
+  select(StockName, SSByr, NfishRec)  |> 
+  left_join(NEbioBstd, by = c("StockName" = "StockName",
+                              "SSByr"="Year"))  |> 
+  select(StockName, SSByr, NfishRec, SSBkg)
+
+
+min_year <- 1980
+max_year <- end.year
+
+
+AssessFishProdAnomaly <- SSrecbio  |> 
+  group_by(StockName) |> 
+  dplyr::mutate(
+    recruits_abund_lead1 = NfishRec,
+    spawners_biom = SSBkg,
+    YEAR = SSByr,
+    rs         = recruits_abund_lead1/
+      spawners_biom,
+    logr_abund = log(recruits_abund_lead1),
+    logs_biom  = log(spawners_biom),
+    logrs      = log(rs),
+    spawners_biom_lag0_anom =
+      (spawners_biom - mean(spawners_biom, na.rm = TRUE)) / sd(spawners_biom, na.rm = TRUE),
+    recruits_abund_lead1_anom =
+      (recruits_abund_lead1 - mean(recruits_abund_lead1, na.rm = TRUE)) / sd(recruits_abund_lead1, na.rm = TRUE),
+    rs_anom       = (rs - mean(rs, na.rm = TRUE)) / sd(rs, na.rm = TRUE),
+    logr_abund_anom =
+      (logr_abund - mean(logr_abund, na.rm = TRUE)) / sd(logr_abund, na.rm = TRUE),
+    logs_biom_anom =
+      (logs_biom - mean(logs_biom, na.rm = TRUE)) / sd(logs_biom, na.rm = TRUE),
+    logrs_anom =
+      (rs - mean(rs, na.rm = TRUE)) / sd(rs, na.rm = TRUE),
+  ) |> 
+  # Remove year before minimum and year after maximum
+  dplyr::filter(YEAR >= min_year,
+                YEAR <= max_year)  |> 
+  #Average seasonal anomalies to get yearly anomaly
+  dplyr::group_by(YEAR, StockName)  |>
   dplyr::summarise(
-    Value = mean(Value, na.rm = TRUE),
-    Units = dplyr::first(Units),
-    Description = dplyr::first(Description),
-    AssessmentYear = max(AssessmentYear, na.rm = TRUE),
-    .groups = "drop"
-  ) |>
-  tidyr::pivot_wider(names_from = Metric, values_from = Value) |>
-  dplyr::rename(
-    spawners_biom_lag0 = Abundance,
-    recruits_abund = Recruitment
-  ) |>
-  # Separate StockName into Stock and Region
-  tidyr::separate(StockName, into = c("Stock", "Region"), sep = "-") |>
-  dplyr::group_by(Stock) |>
-  dplyr::mutate(
-    recruits_abund_lead1 = dplyr::lead(recruits_abund, 1),
-    rs = recruits_abund_lead1 / spawners_biom_lag0,
-    spawners_biom_lag0_anom = (spawners_biom_lag0 - mean(spawners_biom_lag0, na.rm = TRUE)) / sd(spawners_biom_lag0, na.rm = TRUE),
-    recruits_abund_lead1_anom = (recruits_abund_lead1 - mean(recruits_abund_lead1, na.rm = TRUE)) / sd(recruits_abund_lead1, na.rm = TRUE),
-    rs_anom = (rs - mean(rs, na.rm = TRUE)) / sd(rs, na.rm = TRUE),
-    logr_abund_anom = (log(recruits_abund) - mean(log(recruits_abund), na.rm = TRUE)) / sd(log(recruits_abund), na.rm = TRUE),
-    logs_biom_anom = (log(spawners_biom_lag0) - mean(log(spawners_biom_lag0), na.rm = TRUE)) / sd(log(spawners_biom_lag0), na.rm = TRUE),
-    logrs_anom = (log(rs) - mean(log(rs), na.rm = TRUE)) / sd(log(rs), na.rm = TRUE),
-    EPU = dplyr::recode(Region,
-                        " Gulf of Maine / Georges Bank" = "NE",
-                        " Gulf of Maine / Cape Hatteras" = "ALL",
-                        " Mid" = "MA",
-                        " Atlantic Coast" = "ALL",
-                        " Georges Bank" = "NE",
-                        " Northwestern Atlantic Coast" = "ALL",
-                        " Gulf of Maine" = "NE",
-                        " Southern New England / Mid" = "MA",
-                        " Cape Cod / Gulf of Maine" = "NE")
-  ) |>
-  dplyr::ungroup() |>
-  tidyr::pivot_longer(cols = c(
-    "spawners_biom_lag0", "spawners_biom_lag0_anom",
-    "recruits_abund_lead1", "recruits_abund_lead1_anom",
-    "rs","rs_anom",
-    "logr_abund_anom","logs_biom_anom","logrs_anom"
-  ), names_to = "Var", values_to = "Value") |>
-  dplyr::mutate(
-    Var = paste0(Stock, "-", Var, "-Assessment"),
-    Time = Year,
-    Units = NA
-  ) |>
+    spawners_biom_lag0 = mean(spawners_biom, na.rm = T),
+    spawners_biom_lag0_anom = mean(spawners_biom_lag0_anom,
+                                   na.rm = T),
+    recruits_abund_lead1      = mean(recruits_abund_lead1,
+                                     na.rm = T),
+    recruits_abund_lead1_anom = mean(recruits_abund_lead1_anom,
+                                     na.rm = T),
+    rs                 = mean(rs, na.rm = T),
+    rs_anom            = mean(rs_anom, na.rm = T),
+    logr_abund_anom    = mean(logr_abund_anom,
+                              na.rm = T),
+    logs_biom_anom     = mean(logs_biom_anom,
+                              na.rm = T),
+    logrs_anom         = mean(logrs_anom, na.rm = T),
+  )
+
+
+# Back to reproducing ecodata::get_productivity_anomaly.R -------------------
+
+prod_assess1<- AssessFishProdAnomaly |> 
+  tidyr::separate(StockName, into= c("Stock", "Region"), sep = "-") |> 
+  dplyr::mutate(EPU = dplyr::recode(Region, " Gulf of Maine / Georges Bank" = "NE",
+                                    " Gulf of Maine / Cape Hatteras" = "ALL",
+                                    " Mid" = "MA",
+                                    " Atlantic Coast" = "ALL",
+                                    " Georges Bank" = "NE",
+                                    " Northwestern Atlantic Coast" = "ALL",
+                                    " Gulf of Maine" = "NE",
+                                    " Southern New England / Mid" = "MA",
+                                    " Cape Cod / Gulf of Maine" = "NE")) |> 
+  tidyr::pivot_longer(cols = c("spawners_biom_lag0", "spawners_biom_lag0_anom",
+                               "recruits_abund_lead1","recruits_abund_lead1_anom",
+                               "rs","rs_anom","logr_abund_anom",
+                               "logs_biom_anom","logrs_anom"),
+                      names_to = "Var", values_to = "Value") |> 
+  dplyr::mutate(Var = paste0(Stock, "-", Var, "-Assessment"),
+                Time = YEAR,
+                Units = c("NA")) |> 
   dplyr::ungroup() |> 
   dplyr::select(Time, Var, Value, EPU, Units)
 
 
-# Combine survey and assessment data -------------------------
 
-
-productivity_anomaly <- rbind(productivity_anomaly1, prod_assess1) |> 
-  dplyr::filter(Time > 1979)
+# combining into final product ------------------------------
+message("Combining final product")
+productivity_anomaly<- rbind(productivity_anomaly1, prod_assess1)
 
 # Return productivity_anomaly -----------------------
 
