8ef1f8fe49b23abb1619dee60a065ed10de9ea86
Max Grezlik <maxwell.grezlik@noaa.gov>
2025-10-31 13:03:46 -0500

    changed the name of this file to match what I did for comdat

--- data-raw/test_run_productivity_anomaly.R
+++ data-raw/test_run_productivity_anomaly.R
@@ -5,13 +5,15 @@
 input_survey_bio_epu <- "~/EDAB_Datasets/Workflows/surveyBiologicalByEPUData.rds"
 input_survey_bio <- "~/EDAB_Datasets/Workflows/surveyBiologicalData.rds"
 inputPathSpecies <- "~/EDAB_Datasets/Workflows/SOE_species_list_24.rds"
-
-# Add your desired output path here or keep it as Max's folder in EDAB_Dev
-outputPath <- "~/EDAB_Dev/grezlik"
+outputPath <- "~/EDAB_Indicators/productivity_anomaly.rds"
 
 
 ## run workflow ------------------
+
+source(here::here("data-raw/workflow_productivity_anomaly.R"))
+
 test_productivity_anomaly <- workflow_productivity_anomaly(
   input_survey_bio_epu = input_survey_bio_epu,
+  input_survey_bio = input_survey_bio,
   inputPathSpecies = inputPathSpecies
 )
