51302929e9d73954ea87a6bae1ad1be9e0e3b7dc
Max Grezlik <maxwell.grezlik@noaa.gov>
2025-11-26 11:06:27 -0500

    update input params

--- data-raw/workflow_productivity_anomaly.R
+++ data-raw/workflow_productivity_anomaly.R
@@ -5,8 +5,9 @@
 #'
 #' @param input_survey_bio_epu Character string. Full path to the survey data with bio and epu rds file
 #' @param input_survey_bio Character string. Full path to the survey data with bio rds file
-#' @param input_lw_table Character string. Full path to the lenght weight table rda file
+#' @param input_static_lw_table Character string. Full path to the static length weight table rda file
 #' @param inputPathSpecies Character string. Full path to the species list data pull rds file
+#' @param input_static_length_convert Character string. Full path to length conversion rda file
 #' @param outputPath Character string. Path to folder where data pull should be saved
 #'
 #' @example
@@ -15,8 +16,9 @@
 #' workflow_productivity_anomaly(
 #'   input_survey_bio_epu <- "path/to/survey/bio/epu/.rds"
 #'   input_survey_bio <- "path/to/survey/bio/.rds",
-#'   input_lw_table <- "path/to/lw/table/.rda",
+#'   input_static_lw_table <- "path/to/lw/table/.rda",
 #'   inputPathSpecies = "path/to/species/data/.rds",
+#'   input_static_length_convert = "path/to/length/table/.rda"
 #'   outputPath = "path/to/output/folder"
 #'   )
 #' }
@@ -27,15 +29,18 @@
 
 workflow_productivity_anomaly <- function(input_survey_bio_epu,
                                           input_survey_bio,
-                                          input_lw_table,
+                                          input_static_lw_table,
                                           inputPathSpecies,
+                                          input_static_length_convert,
                                           outputPath) {
   
   prod_anom <- SOEworkflows::create_productivity_anomaly(
-                                           input_survey_bio = input_survey_bio, 
-                                           input_survey_bio_epu = input_survey_bio_epu,
-                                           input_lw_table = input_lw_table,
-                                           inputPathSpecies = inputPathSpecies)
+    input_survey_bio_epu = input_survey_bio_epu,
+    input_survey_bio = input_survey_bio,
+    input_static_lw_table = input_lw_table,
+    inputPathSpecies = inputPathSpecies,
+    input_static_length_convert = input_static_length_convert
+    )
   
   saveRDS(prod_anom,
           file = file.path(outputPath, "productivity_anomaly.rds"))
