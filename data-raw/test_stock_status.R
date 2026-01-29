028090c15d9c751717a51daaec4fcfdee5d6faf1
MGrezlik <maxwell.grezlik@noaa.gov>
2026-01-29 12:16:17 -0500

    updated file paths to work on the container

--- data-raw/test_stock_status.R
+++ data-raw/test_stock_status.R
@@ -1,8 +1,10 @@
 
 # dir.create(here::here("data-raw/temp"))
 
-inputPath <- "/home/atyrell/SOE_ESP_Data/READ_EDAB_SOE_Workflows/data-raw/temp/decoder.csv"
-outputPath <- "/home/atyrell/SOE_ESP_Data/READ_EDAB_SOE_Workflows/data-raw/temp"
+inputPath <- '~/EDAB_Dev/grezlik/decoder - Sarah Gaichas - NOAA Federal.csv'
+outputPath <- '~/EDAB_Dev/grezlik'
+# inputPath <- "/home/atyrell/SOE_ESP_Data/READ_EDAB_SOE_Workflows/data-raw/temp/decoder.csv"
+# outputPath <- "/home/atyrell/SOE_ESP_Data/READ_EDAB_SOE_Workflows/data-raw/temp"
 
 source(here::here("data-raw/workflow_stock_status.R"))
 
